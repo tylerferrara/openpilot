770ef366757fdf6110efc8f5701897703f0e4042
Comma Device <device@comma.ai>
2023-04-18 17:23:06 -0500

    install new power drop monitor

--- launch_openpilot.sh
+++ launch_openpilot.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
+if ! cmp --silent /usr/comma/power_drop_monitor.py /data/openpilot/scripts/power_drop_monitor.py; then
+  echo "updating power drop monitor"
+  sudo mount -o rw,remount /
+  sudo cp /data/openpilot/scripts/power_drop_monitor.py /usr/comma/
+  sudo mount -o ro,remount /
+else
+  echo "power drop monitor up to date"
+fi
+
 export PASSIVE="0"
 exec ./launch_chffrplus.sh
 
